6a75e30a059adf25c1664e304c4690da053301f2
Max Asselmeier <masselmeier@ihmc.org>
2022-03-07 10:33:43 -0500

    make_plan running through several nodes, want to condense to one

--- scripts/follow_global_path.py
+++ scripts/follow_global_path.py
@@ -3,18 +3,25 @@ import rospy
 from nav_msgs.srv import GetPlan
 import numpy as np
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Twist, Vector3
+
+import tf_conversions
+
+import tf2_ros
+import tf2_geometry_msgs
 
 class Agent:
-    def __init__(self):
-        rospy.wait_for_service('/move_base/make_plan')
-        self.get_plan = rospy.ServiceProxy('/move_base/make_plan', GetPlan)
+    def __init__(self, robot_namespace):
+        self.robot_namespace = robot_namespace
+        rospy.wait_for_service('/move_base_virtual/make_plan')
+        self.get_plan = rospy.ServiceProxy('/move_base_virtual/make_plan', GetPlan)
         self.plan_idx = 0
         self.x = 0.0
         self.y = 0.0
         self.plan = None
         self.agent_odoms = None
-        self.odom_sub = rospy.Subscriber("robot0/odom", Odometry, self.odom_CB, queue_size=5)
+        self.odom_sub = rospy.Subscriber(robot_namespace + "/odom", Odometry, self.odom_CB, queue_size=5)
+        self.cmd_vel_pub = rospy.Publisher(robot_namespace + "/cmd_vel", Twist, queue_size=5)
         self.start = PoseStamped()
         self.start.header.frame_id = "known_map"
         self.goal = PoseStamped()
@@ -22,20 +29,47 @@ class Agent:
         self.goal.pose.position.y = 10
         self.get_global_plan()
 
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+
     def odom_CB(self, msg):
+        #print('odom msg header', msg.header)
         position = msg.pose.pose.position
         self.x = position.x
         self.y = position.y
+        #print('agent x,y: ', self.x, ', ', self.y)
+        desired_pose = self.plan.plan.poses[self.plan_idx]
+        # ODOM: map_static
+        # DESIRED: known_map
+        # CMD_VEL: robot0 frame
+        #print('desired pose: ', desired_pose)
+        # first is target frame, second is source frame
+        map_static_to_known_map_trans = self.tfBuffer.lookup_transform("known_map", "map_static", rospy.Time())
+        # print(map_static_to_known_map_trans)
+        # transforming from map_static to known_map
+        odom_in_known_map = tf2_geometry_msgs.do_transform_pose(msg.pose, map_static_to_known_map_trans)
+        x_diff = odom_in_known_map.pose.position.x - desired_pose.pose.position.x
+        y_diff = odom_in_known_map.pose.position.y - desired_pose.pose.position.y
+        # transforming from known_map to robot0
+        #print('x_diff: ', x_diff, ', and y_diff: ', y_diff)
+        known_map_to_robot_trans = self.tfBuffer.lookup_transform(robot_namespace, "known_map", rospy.Time())
+        diff_vect = PoseStamped()
+        diff_vect.pose.position.x = -x_diff
+        diff_vect.pose.position.y = -y_diff
+        diff_in_robot_0 = tf2_geometry_msgs.do_transform_pose(diff_vect, known_map_to_robot_trans)
+        twist = Twist()
+        twist.linear.x = self.clip_cmd_vel(diff_in_robot_0.pose.position.x)
+        twist.linear.y = self.clip_cmd_vel(diff_in_robot_0.pose.position.y)
+        # print('twist: ', twist)
+        self.cmd_vel_pub.publish(twist)
 
-        #desired_pose = self.plan[self.plan_idx]
-        #x_diff = self.x - desired_pose.position.x
-        #y_diff = self.y - desired_pose.position.y
-        #cmd_vel_x = -x_diff
-        #cmd_vel_y = -y_diff
-        #if np.sqrt(x_diff ** 2 + y_diff ** 2) < 0.05:
-        #    self.plan_idx += 1
+        if np.sqrt(x_diff ** 2 + y_diff ** 2) < 0.05:
+            self.plan_idx += 1
+            if len(self.plan) < self.plan_idx:
+                self.plan_idx = 0
 
-        #if len(self.plan) < self.plan_idx:
+        # cap agent velocity
+
         #    self.start = 0.0
         #    self.goal = 0.0
         #    self.get_global_plan()
@@ -46,12 +80,23 @@ class Agent:
         req.goal = self.goal
         req.tolerance = 0.5
         self.plan = self.get_plan(req.start, req.goal, req.tolerance)
-        # print(self.plan)
+        # print("trying self.plan.respone: ", self.plan.response)
+        # print("trying self.plan.plan: ", self.plan.plan)
+
+    def clip_cmd_vel(self, original_val):
+        if original_val > 1:
+            return 1
+        elif original_val < -1:
+            return -1
+        else:
+            return original_val
 
 if __name__ == '__main__':
     try:
         rospy.init_node("follow_global_path", anonymous=True)
-        Agent()
+        robot_namespace = rospy.get_param("~robot_namespace")
+        #print("robot namespace: ", robot_namespace)
+        Agent(robot_namespace)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
