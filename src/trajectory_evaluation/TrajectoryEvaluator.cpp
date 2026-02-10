dcb78282e38fc346e398d3cac5a8c2c8217f81ef
abdelzaro <abdelzaro8@gmail.com>
2026-02-10 18:54:11 -0500

    added debugging printouts in traj_evalutor for debugging cbf inputs bc they're not always passed along and getting a nan

--- src/trajectory_evaluation/TrajectoryEvaluator.cpp
+++ src/trajectory_evaluation/TrajectoryEvaluator.cpp
@@ -284,11 +284,11 @@ namespace dynamic_gap
         dwa_traj.robotVel = RbtVel;
 
         
-        // ROS_ERROR_STREAM_NAMED("eval",
-        // "inside traj evaluator right after populating the object:"
-        // << " humanVel=(" << dwa_traj.humanVelLeft.x() << "," << dwa_traj.humanVelLeft.y() << ")"
-        // << " gapPos=(" << dwa_traj.gapPosLeft.x() << "," << dwa_traj.gapPosLeft.y() << ")"
-        // << " robotVel=(" << dwa_traj.robotVel.x() << "," << dwa_traj.robotVel.y() << ")");
+        ROS_ERROR_STREAM_NAMED("eval",
+        "inside traj evaluator right after populating the object:"
+        << " humanVel=(" << dwa_traj.humanVelLeft.x() << "," << dwa_traj.humanVelLeft.y() << ")"
+        << " gapPos=(" << dwa_traj.gapPosLeft.x() << "," << dwa_traj.gapPosLeft.y() << ")"
+        << " robotVel=(" << dwa_traj.robotVel.x() << "," << dwa_traj.robotVel.y() << ")");
 
 
         }
@@ -336,6 +336,12 @@ namespace dynamic_gap
 
             dwa_traj.humanVelRight = rightVel;
             dwa_traj.gapPosRight = rightGapRelPos;
+
+            ROS_ERROR_STREAM_NAMED("eval",
+        "right endpoint ------- inside traj evaluator right after populating the object:"
+        << " humanVelRight=(" << dwa_traj.humanVelRight.x() << "," << dwa_traj.humanVelRight.y() << ")"
+        << " gapPos=(" << dwa_traj.gapPosRight.x() << "," << dwa_traj.gapPosRight.y() << ")");
+        
         }
 
 
